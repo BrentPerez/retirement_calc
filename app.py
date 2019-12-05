814bee4ef673afdab74f68d4a84ac39c155e5bbd
Brent Perez <43828037+Perezinator@users.noreply.github.com>
2019-12-04 21:16:45 -0500

    Scrap and restarted

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 
+from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, usd
+from helpers import apology, login_required, lookup, usd
 
-# Configure app for Flask
+# Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
@@ -17,35 +18,176 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-stre, must-revalidate"
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
-    response.header["Pragma"] = "no-cache"
+    response.headers["Pragma"] = "no-cache"
     return response
 
-# USD filter
+
+# Custom filter
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
 
 
 @app.route("/")
 @login_required
 def index():
-    # Show budget
+    """Show portfolio of stocks"""
+    return apology("TODO", 200)
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        try:
+            shares = int(request.form.get("shares"))
+        except ValueError:
+            return apology("invalid number of shares", 400)
+        pricelook = lookup(symbol)
+        if not pricelook:
+            return apology("must provide valid stock symbol", 400)
+        price = pricelook["price"]
+        charge = price * shares
+        userMoney = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+        if shares <= 0:
+            return apology("invalid number of shares", 400)
+        elif not shares:
+            return apology("invalid number of shares", 400)
+        elif userMoney < charge:
+            return apology("not enough cash in account", 400)
+        return render_template("portfolio.html", charge=charge)
+    else:
+        return render_template("buy.html")
+
+
+@app.route("/check", methods=["GET"])
+def check():
+    """Return true if username available, else false, in JSON format"""
+    return jsonify("TODO")
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
     return apology("TODO")
 
 
-@app.rout("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    # Log user in
-    return render_template(layout.html)
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology(400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/", 200)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+        quoted = lookup(request.form.get("symbol"))
+        if not quoted:
+            return apology("invalid stock symbol", 400)
+        quoted["price"] = usd(quoted["price"])
+        return render_template("quoted.html", quoted=quoted)
+    else:
+        return render_template("quote.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("Missing username!", 400)
+        elif not request.form.get("password"):
+            return apology("Missing password!", 400)
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords did not match", 400)
+        else:
+            username = request.form.get("username")
+            hashpass = generate_password_hash(request.form.get("password"))
+
+            insertUser = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hashpass)",
+                                    username=username, hashpass=hashpass)
+            if not insertUser:
+                return apology("Sorry, Username already taken", 400)
+
+        # Login user after register
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        session["user_id"] = rows[0]["id"]
+        # Return to homepage
+        return redirect("/", 200)
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    return apology("TODO")
 
 
 def errorhandler(e):
+    """Handle error"""
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
--- application.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from helpers import usdapology, login_required, usd
